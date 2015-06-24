a5dc8802df4b95ea4232a35f1834a2c394a5819b
Guilherme G. M <guilherme.menaldo@outlook.com>
2015-06-24 18:03:47 -0300

    10. Creating hazards

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+	public float Tumble;
+
+	private Rigidbody RB;
+
+	void Start()
+	{
+		RB = this.GetComponent<Rigidbody> ();
+
+		RB.angularVelocity = Random.insideUnitSphere * Tumble;
+	}
+}
