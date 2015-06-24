f93324ae0d133dd9b622902992de9a45a596c9fa
Guilherme G. M <guilherme.menaldo@outlook.com>
2015-06-24 19:07:59 -0300

    12. Game Controller

---
+++ Assets/GameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject Hazard;
+	public Vector3 SpawnValues;
+
+	void Start ()
+	{
+		SpawnWaves ();
+	}
+
+	void SpawnWaves()
+	{
+		Vector3 spawnPosition = new Vector3 (Random.Range(-SpawnValues.x, SpawnValues.x), SpawnValues.y, SpawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
+
+		Instantiate (Hazard, spawnPosition, spawnRotation);
+	}
+}
