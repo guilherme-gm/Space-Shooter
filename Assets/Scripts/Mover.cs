1dfaa1385d4a7ce6f9e9f434106af8928e9e251f
Guilherme G. M <guilherme.menaldo@outlook.com>
2015-06-24 17:13:38 -0300

    - Adicionado arquivos que faltavam no commit anterior

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	public float Speed;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody> ().velocity = transform.forward * Speed;
+	}
+}
